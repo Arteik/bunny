9e3ab1b9d5f7fa571514a5389b17629326e8c276
Shomik Ghose <tigger.ghose@gmail.com>
2025-04-26 00:22:31 -0400

    Distributed slices work!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
+use std::fmt::Debug;
 
+use bunny::{BunnyArgs, BunnyFunction};
 use linkme::distributed_slice;
 use serde::{Deserialize, Serialize};
-use warp::Filter;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct RawQuery {
-    /// Raw query string; ?@decorator cmd ?[..args]
-    q: String,
-}
+use warp::{reply::Reply, Filter};
 
 pub mod bunny {
     use super::*;
 
-    #[derive(Serialize, Deserialize, Debug)]
+    #[derive(Serialize, Deserialize, Debug, Clone)]
     pub struct BunnyArgs {
         /// Modifies BunnyAction behavior; enables targeting
         pub decorator: String,
@@ -51,234 +47,122 @@ pub mod bunny {
         }
     }
 
-    // pub trait BunnyAction {
-    //     fn execute(args: &BunnyArgs) -> Box<dyn warp::Reply>;
-    // }
+    pub trait BunnyAlias {
+        fn aliases(&self) -> &'static [&'static str];
+    }
+    
+    pub type BunnyFunction = Box<dyn Fn(BunnyArgs) -> Box<dyn Reply + 'static> + Send + Sync>;
+    pub trait BunnyAction {
+        fn execute(&self, args: bunny::BunnyArgs) -> Box<dyn warp::Reply>;
+    }
+    
+    pub trait BunnyCommand: BunnyAlias + BunnyAction {}
 
-    // pub trait SimpleRedirectBunnyAction: BunnyAction {
-    //     fn build_uri(args: &BunnyArgs) -> warp::http::Uri;
-    // }
-
-    // impl<T: SimpleRedirectBunnyAction> BunnyAction for T {
-    //     fn execute(args: &BunnyArgs) -> Box<dyn warp::Reply> {
-    //         Box::new(warp::redirect(Self::build_uri(args)))
-    //     }
-    // }
-
-    // pub trait TemplatedRedirectBunnyAction: SimpleRedirectBunnyAction {
-    //     const TEMPLATE: &'static str = "{}";
-    // }
-
-    // impl<T: TemplatedRedirectBunnyAction> SimpleRedirectBunnyAction for T {
-    //     fn build_uri(args: &BunnyArgs) -> warp::http::Uri {
-    //         let x = T::TEMPLATE.format(&[args.args.clone()]);
-    //         warp::http::Uri::from_str(&x).expect("foo")
-    //     }
-    // }
+    impl<T: BunnyAlias + BunnyAction + Send + Sync + Debug> BunnyCommand for T {}
 }
+
+pub fn build_command_map(
+) -> HashMap<
+    &'static str,
+    BunnyFunction
+> {
+    COMMANDS
+        .iter()
+        .flat_map(|cmd| {
+            let aliases = cmd().aliases();
+            aliases.iter().map(move |&alias| {
+                let handler: BunnyFunction
+                    = Box::new(move |args: BunnyArgs| {
+                        cmd().execute(args)
+                    });
+                (alias, handler)
+            })
+        })
+        .collect()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RawQuery {
+    /// Raw query string; ?@decorator cmd ?[..args]
+    q: String,
+}
+
+#[distributed_slice]
+pub static COMMANDS: [fn() -> Box<dyn crate::bunny::BunnyCommand>];
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-
     let log = warp::log("rustybunny::main");
+
+    let command_map = Arc::new(build_command_map());
+
+    let with_map = warp::any()
+        .map({
+            let map_clone  = command_map.clone();
+            move || map_clone.clone()
+        });
 
     let example1 = warp::get()
         .and(warp::path("bunny"))
         .and(warp::query::<RawQuery>())
-        .map(|p: RawQuery| p.q);
+        .and(with_map) 
+        .map(move |p: RawQuery, cmd_map: Arc<HashMap<&str, BunnyFunction>>| {
+            let args = BunnyArgs::from(p);
+
+            if let Some(execute_fn) = cmd_map.get(args.cmd.as_str()) {
+                execute_fn(args)
+            } else {
+                Box::new(String::from("Can't find this command!"))
+            }
+        });
 
     let hello_world = warp::get()
         .and(warp::path::end())
         .map(|| "Hello, World at root!")
         .with(log);
 
-    let routes = hello_world.or(example1);
-    // .or(example1);
+    let routes = hello_world
+        .or(example1);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-trait BunnyAlias {
-    fn aliases(&self) -> &'static [&'static str];
-}
+mod youtube {
+    use linkme::distributed_slice;
 
-trait BunnyAction {
-    fn execute(&self, args: bunny::BunnyArgs) -> Box<dyn warp::Reply>;
-}
+    use crate::bunny::{BunnyAction, BunnyAlias, BunnyArgs, BunnyCommand};
 
-trait BunnyCommand: BunnyAlias + BunnyAction {}
- 
-impl<T: BunnyAlias + BunnyAction + Send + Sync> BunnyCommand for T {}
-
-#[distributed_slice]
-pub static COMMANDS: [Box<dyn BunnyCommand>];
-
-struct Youtube;
-
-static FOO: Box<dyn BunnyCommand + Send + Sync> = Box::new(Youtube::new());
-
-impl Youtube {
-    const fn new() -> Self {
-        Self
+    #[distributed_slice(crate::COMMANDS)]
+    fn register() -> Box<dyn BunnyCommand> {
+        Box::new(Youtube::new())
     }
-}
 
-impl BunnyAlias for Youtube {
-    fn aliases(&self) -> &'static [&'static str] {
-        &["youtube", "yt"]
+    #[derive(Debug)]
+    struct Youtube;
+
+    impl Youtube {
+        const fn new() -> Self {
+            Self
+        }
     }
-}
 
-impl BunnyAction for Youtube {
-    fn execute(&self, args: bunny::BunnyArgs) -> Box<dyn warp::Reply> {
-        Box::new(format!("www.youtube.com/results?search_query={}", args.args))
+    impl BunnyAlias for Youtube {
+        fn aliases(&self) -> &'static [&'static str] {
+            &["youtube", "yt"]
+        }
     }
+
+    impl BunnyAction for Youtube {
+        fn execute(&self, args: BunnyArgs) -> Box<dyn warp::Reply> {
+            let uri = warp::http::Uri::builder()
+                .scheme("https")
+                .authority("youtube.com")
+                .path_and_query(format!("/results?search_query={}", args.args))
+                .build()
+                .unwrap();
+            Box::new(warp::redirect::temporary(uri))
+        }
+    }
+
 }
-
-// trait BunnyAlias {
-//     fn aliases() -> &'static [&'static str];
-// }
-
-// trait BunnyAction {
-//     fn execute(args: bunny::BunnyArgs) -> impl warp::Reply;
-// }
-
-// trait BunnyCommand: BunnyAlias + BunnyAction {
-//     fn register<F>(
-//         map: &mut HashMap<&'static str, F>,
-//     ) where F: Fn(bunny::BunnyArgs) -> Box<dyn warp::Reply> + 'static;
-// }
-
-// impl<T: BunnyAlias + BunnyAction> BunnyCommand for T {
-//     fn register<F>(
-//         map: &mut HashMap<&'static str, F>,
-//     ) where F: Fn(bunny::BunnyArgs) -> Box<dyn warp::Reply> + 'static {
-//         for alias in T::aliases() {
-//             let x = |args| Box::new(T::execute(args));
-//             map.insert(*alias, x);
-//         }
-//     }
-// }
-
-// // #[distributed_slice]
-// // pub static COMMANDS: [Fn(
-// //     &mut HashMap<&'static str, Box<dyn Fn(bunny::BunnyArgs) -> Box<dyn warp::Reply>>>,
-// // )];
-
-// struct Youtube;
-
-// impl BunnyAlias for Youtube {
-//     fn aliases() -> &'static [&'static str] {
-//         &["youtube", "yt"]
-//     }
-// }
-
-// impl BunnyAction for Youtube {
-//     fn execute(args: bunny::BunnyArgs) -> impl warp::Reply {
-//         format!("www.youtube.com/results?search_query={}", args.args)
-//     }
-// }
-
-// struct Youtube;
-// impl bunny::TemplatedRedirectBunnyAction for Youtube {
-//     const TEMPLATE: &'static str = "https://www.youtube.com/results?search_query={}";
-// }
-
-// // Define the macro
-// macro_rules! define_commands {
-//     ($($cmd_name:ident => $closure:expr),*) => {
-//         // Define the trait
-//         trait BunnyCommand {
-//             fn execute(&self, args: BunnyArgs);
-//         }
-
-//         // Define the structs and their implementations
-//         $(
-//             struct $cmd_name;
-
-//             impl BunnyCommand for $cmd_name {
-//                 fn execute(&self, args: BunnyArgs) {
-//                     $closure(args);
-//                 }
-//             }
-//         )*
-
-//         // Define the enum
-//         enum BunnyCommands {
-//             $($cmd_name),*
-//         }
-
-//         // Implement the Command trait for the enum
-//         impl BunnyCommand for BunnyCommands {
-//             fn execute(&self, args: BunnyArgs) {
-//                 match self {
-//                     $(
-//                         BunnyCommands::$cmd_name => cmd.execute(args),
-//                     )*
-//                 }
-//             }
-//         }
-//     };
-// }
-
-// // Use the macro to define the commands and the enum
-// define_commands!(
-//     Youtube => |_args: BunnyArgs| todo!(),
-// );
-
-// // Define the macro
-// macro_rules! define_commands {
-//     ($($cmd_name:ident => $closure:expr),*) => {
-//         // Define the trait
-//         trait BunnyCommand {
-//             fn build_action(&self, args: BunnyArgs);
-//         }
-
-//         // Define the structs and their implementations
-//         $(
-//             struct $cmd_name;
-
-//             impl BunnyCommand for $cmd_name {
-//                 fn build_action(&self, _args: BunnyArgs) {
-//                     $closure;
-//                 }
-//             }
-//         )*
-
-//         // Define the enum
-//         enum BunnyCommands {
-//             $($cmd_name($cmd_name)),*
-//         }
-
-//         // Implement the Command trait for the enum
-//         impl BunnyCommand for BunnyCommands {
-//             fn build_action(&self, args: BunnyArgs) {
-//                 match self {
-//                     $(
-//                         BunnyCommands::$cmd_name(cmd) => cmd.build_action(args),
-//                     )*
-//                 }
-//             }
-//         }
-//     };
-// }
-
-// // Use the macro to define the commands and the enum
-// define_commands!(
-//     Youtube => {
-//         |args: BunnyArgs| Response::builder().body(format!(
-//             "decorator = {}, cmd = {}, args = {:#?}",
-//             args.decorator, args.cmd, args.args
-//         ));
-//     }
-// );
-
-// struct TemplatedRedirectBunnyAction {
-//     url: 'static
-// }
-
-// enum BunnyCommands {
-//     Youtube(TemplatedRedirectBunnyAction)
-
-// }
